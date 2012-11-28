e634b8af1cefb48b05bf34adba20c30c8ea06fd9
Joseph Lenton <josephlenton@studiofortress.com>
2012-11-28 20:27:12 +0000

    added incognito mode on startup

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-start .\chrome\chrome.exe --allow-file-access --allow-file-access-from-files --disable-web-security --enable-experimental-extension-apis --app="file:%~dp0/src/index.html"
+start .\chrome\chrome.exe --allow-file-access --allow-file-access-from-files --disable-web-security --enable-experimental-extension-apis --app="file:%~dp0/src/index.html" -incognito
 
